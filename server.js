6deb7b6ebb8367459c78dc208287dcc873db6c59
Aben25 <51212800+Aben25@users.noreply.github.com>
2021-07-28 11:44:05 -0400

    Week 1 Workshop Assignment

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter')
 
 
 const hostname = 'localhost';
@@ -11,36 +13,14 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+
+
 
 app.use(express.static(__dirname + '/public'));
 
-
-
-app.get('/campsites/:campsiteId', (req, res) => {
-    res.end(`will send ditails of campsite: ${req.params.campsiteId} to you`)
-})
-
-app.post('/campsites/:campsiteId', (req, res) => {
-
-    res.statusCode = 403;
-    res.end(`POST operation id forbidden on ${req.params.campsiteId} `)
-})
-
-app.put('/campsites/:campsiteId', (req, res) => {
-
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`)
-    res.end(`Will update the campsite: ${req.body.name} with  description ${req.body.description} `)
-})
-
-app.delete('/campsites/:campsiteId', (req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`)
-})
-
-
-
-
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
